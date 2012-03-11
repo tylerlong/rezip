21c2d5365c2ea66c717fa9b4ea82c2306d1e157b
Tyler Long <tyler4long@gmail.com>
2012-03-11 05:50:32 -0700

    finished the ruby version, but it runs too slowly

--- init/ruby/init.rb
+++ init/ruby/init.rb
@@ -1,6 +1,7 @@
 require "zip/zip"
 
-working_folder = "/home/tyler/Documents"
+#change this to where you store the input folder and output folder
+working_folder = "/home/tyler/Documents" 
 
 if !Dir.exists?(working_folder)
     Dir.mkdir(working_folder)
@@ -16,23 +17,25 @@ if !Dir.exists?(output_folder)
     Dir.mkdir(output_folder)
 end
 
-10.times do |i|
+5.times do |i|
     sub_folder = "#{input_folder}/folder#{i}"
     if !Dir.exists?(sub_folder)
         Dir.mkdir(sub_folder)
     end
 end
 
+#create a data file
 data_file = File.new("#{input_folder}/data.txt", "w")
 data = ([(["abc"] * 20).join(" ")] * rand(500)).join("\n")
 data_file.syswrite(data)
 
-10.times do |i| #for every sub folder in input folder
+#create all of the zip files
+5.times do |i| #for every sub folder in input folder
     sub_folder = "#{input_folder}/folder#{i}"
-    rand(100).times do |j| #for each zip file
+    rand(50).times do |j| #for each zip file
         zip_file = "#{sub_folder}/#{j}.zip"
         Zip::ZipFile.open(zip_file, Zip::ZipFile::CREATE) do |zip_file|
-            rand(1000).times do |k| #for each data file inside a zip file
+            rand(500).times do |k| #for each data file inside a zip file
                 zip_file.add("#{k}.txt", data_file) 
             end
         end 
--- ruby/main.rb
+++ ruby/main.rb
@@ -1,16 +1,34 @@
 require "zip/zip"
 
-source_folder = "/root/source"
-destination_folder = "/root/destination"
+working_folder = "/home/tyler/Documents"
+input_folder = "#{working_folder}/input"
+output_folder = "#{working_folder}/output"
 
-def all_zip_files(folder)
-    Dir.glob("#{folder}/**/*.zip")
+temp_folder = "#{working_folder}/temp"
+if !Dir.exists?(temp_folder)
+    Dir.mkdir(temp_folder)
 end
 
-
-all_zip_files(source_folder).each do |file|
-    puts file
-    Zip::ZipFile.open(file) do |source_zip|
-       source_zip.extract(destination_folder)
+count = 0
+Dir.glob("#{input_folder}/**/*.zip").each do |zip_file| #for every zip file from input folder
+    Zip::ZipFile.open(zip_file) do |zip_file| 
+        zip_file.each do |entry| #for every data file in the zip file
+            entry.extract("#{temp_folder}/#{count}.txt") #unzip
+            count += 1
+        end        
     end
 end
+
+count = 0
+zip_file = Zip::ZipFile.open("#{output_folder}/#{count}.zip", true)
+Dir.glob("#{temp_folder}/*").each do |data_file|
+    zip_file.add(File.basename(data_file), data_file)
+    zip_file.commit
+    File.delete(data_file)
+    if File.size("#{output_folder}/#{count}.zip") > 1024 * 480
+        count += 1
+        zip_file.close
+        zip_file = Zip::ZipFile.open("#{output_folder}/#{count}.zip", true)
+    end
+end
+zip_file.close
